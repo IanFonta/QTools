44dfecf15736d6c78ad55fa5321c25cf519de99a
IanFonta <86026694+IanFonta@users.noreply.github.com>
2024-04-29 19:32:07 -0300

    Added new sections

--- Web/QrGenerator/qr.js
+++ Web/QrGenerator/qr.js
@@ -1,15 +1,38 @@
-function generarQR() {
-    var texto = document.getElementById('textoInput').value;
-    var qrCodeDiv = document.getElementById('qrCode');
-    qrCodeDiv.innerHTML = ''; // Limpiar cualquier QR Code anterior
+var qr;
 
-    if (texto.trim() !== '') {
-        var qrCode = new QRCode(qrCodeDiv, {
+function generarQR() {
+    var texto = document.getElementById("textoInput").value;
+    if (texto) {
+        if (qr) {
+            qr.clear(); // Borrar el QR anterior
+            document.getElementById("qrCode").innerHTML = ''; // Eliminar el contenedor del QR anterior
+        }
+        
+        qr = new QRCode(document.getElementById("qrCode"), {
             text: texto,
             width: 200,
             height: 200
         });
+
+        document.getElementById("textoQR").innerHTML = "<p>" + texto + "</p>";
     } else {
-        qrCodeDiv.innerHTML = '<p>Por favor, ingrese un texto válido.</p>';
+        alert("Por favor, ingrese un texto o URL");
     }
+}
+
+function descargarQR() {
+    if (!qr) {
+        alert("Primero genera el código QR");
+        return;
+    }
+
+    var canvas = document.getElementsByTagName('canvas')[0];
+    var url = canvas.toDataURL("image/png");
+
+    var link = document.createElement("a");
+    link.href = url;
+    link.download = "codigo_qr.png";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
 }
---
+++ Web/parts/nav.php
@@ -0,0 +1,34 @@
+<header>
+        <div class="navbar">
+            <div class="nav-logo">
+                <a href="#"><img id="logo" src="/Web/QrGenerator/Img/Título.png" alt="Logo"></a>
+            </div>
+            <div class="nav-items">
+                <ul>
+                    <li><a href="#" class="architects-daughter-regular"> Inicio </a></li>
+                    <li><a href="#" id="mostrar-seccion" class="architects-daughter-regular"> Herramientas </a></li>
+                    <li><a href="#" class="architects-daughter-regular"> Contacto </a></li>
+                </ul>
+            </div>
+            <div class="nav-button">
+                <div class="anim-layer"></div>
+                <a href="https://github.com/IanFonta"><i class="fa-brands fa-github" id="github" style="padding: 10px 10px;"></i>GitHub</a>
+            </div>
+            <div id="hamburger-menu">&#9776;</div>
+        </div>
+    
+        <div id="mobile-menu">
+            <div class="mobile-nav-items">
+                <ul>
+                    <li><a href="#" class="architects-daughter-regular"> Inicio </a></li>
+                    <li><a href="#" class="architects-daughter-regular"> Herramientas </a></li>
+                    <li><a href="#" class="architects-daughter-regular"> Contacto </a></li>
+                </ul>
+            </div>
+            <div class="mobile-nav-button">
+                <div class="anim-layer"></div>
+                <a href="#">Github</a>
+            </div>
+            <div id="hamburger-cross">&#10006;</div>
+        </div>
+    </header>
